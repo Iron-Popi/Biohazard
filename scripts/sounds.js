0e07a876357b3bbf37567e7caa23945987016b2d
Pauline Bellaud <pauline.bellaud@gmail.com>
2021-02-18 14:16:14 +0100

    audio src fixed?

--- scripts/sounds.js
+++ scripts/sounds.js
@@ -6,17 +6,17 @@ function myPlay(){
 }
 
 function soundEffect1(){
-    let sound = new Audio("../sounds/Zombie Growl.mp3");
+    let sound = new Audio("../../sounds/Zombie Growl.mp3");
     sound.play();
 }
 
 function soundEffect2(){
-    let sound = new Audio("../sounds/Scream Female.mp3");
+    let sound = new Audio("../../sounds/Scream Female.mp3");
     sound.play();
 }
 
 function soundEffect3(){
-    let sound = new Audio("../sounds/Zombie Dog Snarls.mp3");
+    let sound = new Audio("../../sounds/Zombie Dog Snarls.mp3");
     sound.play();
 }
 
